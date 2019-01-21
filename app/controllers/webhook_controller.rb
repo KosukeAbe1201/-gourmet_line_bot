1793716d7313befb90a58f34b294de551e51fd17
Kosuke Abe <kousuke1201abe@gmail.com>
2019-01-22 07:25:20 +0900

    add when rest is null

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -26,139 +26,147 @@ class WebhookController < ApplicationController
           client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Location
           rest = Gurunavi::return_rest(event.message['latitude'], event.message['longitude'])
-          messages = [
-            {
-            type: "flex",
-            altText: "this is a flex message",
-            contents: {
-              type: "bubble",
-              hero: {
-                type: "image",
-                url: rest["image_url"]["shop_image1"] == "" ? "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png" : rest["image_url"]["image_url"],
-                size: "full",
-                aspectRatio: "20:13",
-                aspectMode: "cover",
-                action: {
-                  type: "uri",
-                  uri: return_unknown_if_blank(rest["url_mobile"])
-                }
-              },
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "text",
-                    text: return_unknown_if_blank(rest["name"]),
-                    weight: "bold",
-                    size: "xl"
+          if rest
+            messages = [
+              {
+                type: "flex",
+                altText: "this is a flex message",
+                contents: {
+                  type: "bubble",
+                  hero: {
+                    type: "image",
+                    url: rest["image_url"]["shop_image1"] == "" ? "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png" : rest["image_url"]["image_url"],
+                    size: "full",
+                    aspectRatio: "20:13",
+                    aspectMode: "cover",
+                    action: {
+                      type: "uri",
+                      uri: return_unknown_if_blank(rest["url_mobile"])
+                    }
                   },
-                  {
+                  body: {
                     type: "box",
                     layout: "vertical",
-                    margin: "lg",
-                    spacing: "sm",
                     contents: [
                       {
-                        type: "box",
-                        layout: "baseline",
-                        spacing: "sm",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "PLACE",
-                            color: "#aaaaaa",
-                            size: "sm",
-                            flex: 1
-                          },
-                          {
-                            type: "text",
-                            text: return_unknown_if_blank(rest["address"]),
-                            wrap: true,
-                            color: "#666666",
-                            size: "sm",
-                            flex: 5
-                          }
-                        ]
+                        type: "text",
+                        text: return_unknown_if_blank(rest["name"]),
+                        weight: "bold",
+                        size: "xl"
                       },
                       {
                         type: "box",
-                        layout: "baseline",
+                        layout: "vertical",
+                        margin: "lg",
                         spacing: "sm",
                         contents: [
                           {
-                            type: "text",
-                            text: "TIME",
-                            color: "#aaaaaa",
-                            size: "sm",
-                            flex: 1
+                            type: "box",
+                            layout: "baseline",
+                            spacing: "sm",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "PLACE",
+                                color: "#aaaaaa",
+                                size: "sm",
+                                flex: 1
+                              },
+                              {
+                                type: "text",
+                                text: return_unknown_if_blank(rest["address"]),
+                                wrap: true,
+                                color: "#666666",
+                                size: "sm",
+                                flex: 5
+                              }
+                            ]
                           },
                           {
-                            type: "text",
-                            text: return_unknown_if_blank(rest["opentime"]),
-                            wrap: true,
-                            color: "#666666",
-                            size: "sm",
-                            flex: 5
-                          }
-                        ]
-                      },
-                      {
-                        type: "box",
-                        layout: "baseline",
-                        spacing: "sm",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "TEL",
-                            color: "#aaaaaa",
-                            size: "sm",
-                            flex: 1
+                            type: "box",
+                            layout: "baseline",
+                            spacing: "sm",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "TIME",
+                                color: "#aaaaaa",
+                                size: "sm",
+                                flex: 1
+                              },
+                              {
+                                type: "text",
+                                text: return_unknown_if_blank(rest["opentime"]),
+                                wrap: true,
+                                color: "#666666",
+                                size: "sm",
+                                flex: 5
+                              }
+                            ]
                           },
                           {
-                            type: "text",
-                            text: return_unknown_if_blank(rest["tel"]),
-                            wrap: true,
-                            color: "#666666",
-                            size: "sm",
-                            flex: 5
+                            type: "box",
+                            layout: "baseline",
+                            spacing: "sm",
+                            contents: [
+                              {
+                                type: "text",
+                                text: "TEL",
+                                color: "#aaaaaa",
+                                size: "sm",
+                                flex: 1
+                              },
+                              {
+                                type: "text",
+                                text: return_unknown_if_blank(rest["tel"]),
+                                wrap: true,
+                                color: "#666666",
+                                size: "sm",
+                                flex: 5
+                              }
+                            ]
                           }
                         ]
                       }
                     ]
-                  }
-                ]
-              },
-              footer: {
-                type: "box",
-                layout: "vertical",
-                spacing: "sm",
-                contents: [
-                  {
-                    type: "button",
-                    style: "link",
-                    height: "sm",
-                    action: {
-                      type: "uri",
-                      label: "ぐるなび",
-                      uri: return_unknown_if_blank(rest["url_mobile"])
-                    }
                   },
-                  {
-                    type: "spacer",
-                    size: "sm"
+                  footer: {
+                    type: "box",
+                    layout: "vertical",
+                    spacing: "sm",
+                    contents: [
+                      {
+                        type: "button",
+                        style: "link",
+                        height: "sm",
+                        action: {
+                          type: "uri",
+                          label: "ぐるなび",
+                          uri: return_unknown_if_blank(rest["url_mobile"])
+                        }
+                      },
+                      {
+                        type: "spacer",
+                        size: "sm"
+                      }
+                    ],
+                    flex: 0
                   }
-                ],
-                flex: 0
+                }
+              },
+              {
+                type: 'text',
+                text: "このお店、どうかな？"
               }
-            }
-          },
-          {
-            type: 'text',
-            text: "このお店、どうかな？"
-          }
-          ]
+            ]
             client.reply_message(event['replyToken'], messages)
+          else
+            message = {
+              type: 'text',
+              text: "この近くにお店はないみたいね…"
+            }
+            client.reply_message(event['replyToken'], message)
+          end
         end
       end
     }
